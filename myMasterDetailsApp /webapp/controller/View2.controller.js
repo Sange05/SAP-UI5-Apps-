b125c48bf4e3bc6d2d600bb12a913ba50048d6c1
Sanjeev Vasant Bhogan <bhogansanjeev@gmail.com>
2024-08-29 17:55:16 +0000

    commit

--- myMasterDetailsApp /webapp/controller/View2.controller.js
+++ myMasterDetailsApp /webapp/controller/View2.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-    'ui5/apps/controller/BaseController'],
+    'ui5/apps/controller/BaseController',
+    'sap/m/MessageBox',
+    'sap/m/MessageToast' ],
 
-function(Controller) {
+function(Controller, MessageBox, StatusMessage) {
     'use strict';
 
     return Controller.extend("ui5.apps.controller.View2",{
@@ -10,7 +12,31 @@ function(Controller) {
 
             this.getAppObject().to("idView1");
 
-         
+        },
+
+        popClose: function(choice){
+
+            if(choice === 'OK'){
+
+                var oClose = this.getView().byId("idMoreInfo");
+                oClose.setVisible(false);
+
+                StatusMessage.show("Your Request has been approved successfully!");
+
+            }
+        },
+
+        onAccept: function(){
+            MessageBox.confirm("Would like to approve?",{
+                title:"Confirm Action",
+                // onClose: [this.popClose, this]
+                onClose: this.popClose.bind(this)
+
+            });
+             
+        },
+
+        onReject: function(){
 
         }
 
